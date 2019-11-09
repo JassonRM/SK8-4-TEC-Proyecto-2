a77b23be2b929a15c11e850cb1f4732b4c030eb1
Marco Herrera <m.herrera0799@gmail.com>
2019-11-09 08:49:51 -0600

    Add timeout demo

---
+++ SQL-Server/Concurrency/Timeout2.sql
@@ -0,0 +1,17 @@
+BEGIN TRAN
+    SET LOCK_TIMEOUT 5000;
+    PRINT 'Update Empleado'
+    BEGIN TRY
+        UPDATE Empleado
+        SET Salario=1500058
+        WHERE IdEmpleado = 5
+        PRINT 'Waiting 20 sec'
+        WAITFOR DELAY '00:00:20'
+        PRINT 'Commit'
+        COMMIT
+    END TRY
+    BEGIN CATCH
+        PRINT 'Error'
+        PRINT ERROR_MESSAGE()
+        ROLLBACK
+    END CATCH
